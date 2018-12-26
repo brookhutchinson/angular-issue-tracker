d8a0feadbf02f9eaee4ca34a3bf55a9d4d5fb787
Brook Hutchinson <Brookhutchinson@outlook.com>
2018-12-26 16:13:52 -0600

    Setup Angular Material Use npm to download and install npm package named @angular/material as a runtime dependency Register BrowserAnimationsModule with AppModule Import BrowserAnimationsModule into AppModule Add BrowserAnimationsModule to AppModule @NgModule Decorator "imports" property array Register MatToolbarModule with AppModule Import MatToolbarModule into AppModule Add MatToolbarModule to AppModule @NgModule Decorator "imports" property array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 // modules
-import { NgModule }         from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule }    from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule }                from '@angular/core';
+import { AppRoutingModule }        from './app-routing.module';
+import { BrowserModule }           from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule }        from '@angular/common/http';
 
 // components
-import { AppComponent }     from './app.component';
+import { AppComponent }            from './app.component';
 
 @NgModule({
   // components
   declarations: [ AppComponent ],
   // modules
-  imports: [ AppRoutingModule, BrowserModule, HttpClientModule ],
+  imports: [ AppRoutingModule, BrowserAnimationsModule, BrowserModule, HttpClientModule ],
   // services
   providers: [],
   // bootstrap
