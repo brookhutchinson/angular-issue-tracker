e0991494df7dcd59bc92081bcb745941d49c2bff
Brook Hutchinson <Brookhutchinson@outlook.com>
2018-12-26 20:08:58 -0600

    Setup and Configure Angular Router Create Router Config File named app-routing.module.ts Import ListItemComponent, CreateItemComponent, EditItemComponent into AppRoutingModule Define routes used by router inside AppRoutingModule Define ListIssueComponent Route Define CreateIssueComponent Route Define EditIssueComponent Route Define Default Route Add RouterModule to AppRoutingModule @NgModule Decorator "imports" property array Configure the router by using the "routes" property and the forRoot() method on the RouterModule that is defined inside the AppRoutingModule @NgModule Decorator "imports" property array Export AppRoutingModule from app-routing.module.ts Register AppRoutingModule with AppModule Import AppRoutingModule into AppModule Add AppRoutingModule to AppModule @NgModule Decorator property array Define <router-outlet> directive inside AppComponent HTML Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,22 @@
 import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// components
+import { ListIssueComponent }   from './components/list-issue/list-issue.component';
+import { CreateIssueComponent } from './components/create-issue/create-issue.component';
+import { EditIssueComponent }   from './components/edit-issue/edit-issue.component';
+
 // define routes
-const routes: Routes = [];
+const routes: Routes = [
+  // create issue route
+  { path: 'create-issue', component: CreateIssueComponent },
+  // edit issue route
+  { path: 'edit-issue/:id', component: EditIssueComponent },
+  // list issue route
+  { path: 'list-issue', component: ListIssueComponent },
+  // default route
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   // imports
